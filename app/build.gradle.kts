296a58c80fc5b68612e85cdac3f46be25b87f9f8
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-08 18:24:18 +0300

    Update version code and name for app release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.newsme.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.5-real-backend"
+        versionCode = 7
+        versionName = "1.6-fix-cookie"
     }
     buildTypes { release { isMinifyEnabled = false } }
     compileOptions {
